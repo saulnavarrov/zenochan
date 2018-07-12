a0af9c73ea74f6262ebc82541eb917c1a601c40f
saulnavarrov <sinavarrov@gmail.com>
2018-07-12 16:59:45 -0500

    agregando rutas para webhooks

--- api/controllers/WebhooksController.js
+++ api/controllers/WebhooksController.js
@@ -211,18 +211,9 @@ module.exports = {
             client.sendMessage(String(s.idClient), {
               text: `Response: ${s.text}`,
             });
+            
           }
 
-          // buton
-          client.sendMessage(String(s.idClient), {
-            template_type: 'button',
-            text: `Response btn: ${s.text}`,
-            buttons: [{
-              type: 'postback',
-              title: 'Start Chatting',
-              payload: 'USER_DEFINED_PAYLOAD',
-            }, ],
-          });
         }
         // Retorno de ok para el sistema de facebook
         return res.ok('EVENT_RECEIVED');
