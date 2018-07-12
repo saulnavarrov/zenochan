fda2a778cb207af8aa552b16baaf21ed747e7597
saulnavarrov <sinavarrov@gmail.com>
2018-07-12 15:04:06 -0500

    agregando rutas para webhooks

--- api/controllers/WebhooksController.js
+++ api/controllers/WebhooksController.js
@@ -193,8 +193,8 @@ module.exports = {
         s.idClient = st.sender.id;
         s.idPage = en.id;
         // s.seq = ms.seq;
-        s.txt = typeof (ms.text) === 'undefined'? false : true;
-        s.text = !s.txt ? null : ms.text;
+        // s.txt = typeof (ms.text) === 'undefined'? false : true;
+        // s.text = !s.txt ? null : ms.text;
         // s.type = txt ? 'text' : att.type;
         // s.stiker = txt ? null : typeof (att.payload.sticker_id) === 'undefined' ? null : att.payload.sticker_id;
         // s.image = s.type !== "image" ? '' : att.payload.url;
