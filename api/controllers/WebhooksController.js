101cd60b3ee57e87a4f7af09932a5165e0ec0ca2
saulnavarrov <sinavarrov@gmail.com>
2018-07-15 11:39:58 -0500

    controller save messages

--- api/controllers/WebhooksController.js
+++ api/controllers/WebhooksController.js
@@ -168,8 +168,8 @@ var SaveReadMessage = async function (opt, s, body) {
     console.log('= = = = = ==============> Error', JSON.stringify(reads))
 
   // Actualiza el Mensaje que se ha Leido como tal.
-  if (JSON.stringify(reads) !== []) {
-    var readMes = typeof (reads[0].read) === 'undefined' ? 1 : reads[0].read;
+  if (reads) {
+    var readMes = typeof (reads) === 'undefined' ? 1 : reads[0].read;
     // console.log('= = = = = ==============> ', readMes)
     var readUpdate = await MessengerMessages
       .update({
