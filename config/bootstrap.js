75eac5c5cff39fb492566a01b7a411ba31d9c001
saulnavarrov <sinavarrov@gmail.com>
2018-07-21 13:01:06 -0500

    controller WebHooksController
    Funcion de Filtros de mensajes y Contador de palabras para verificación del texto correcto

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -27,6 +27,32 @@ module.exports.bootstrap = async function(done) {
   // ]);
   // ```
 
+  
+  // var page = await Pages.create({
+  //   idPage: '2083506518327974 ',
+  //   namePage: 'SaulNavarrov',
+  //   active: 'true',
+  //   tokenPage: 'EAAFPdRVJsDoBAClzsa0N0RqrFCYqpcLdDKFNPn2XtTAoDYLwhZCocKO0ZBchlNpZC3CNr1DZA8cWYWhA4RjsYIpJPWHmhNjFxAq166I6ZCP1XRFob6HcJ6C95JWBjX51F69tRUOZBvtcX07Wa8bshrLSryaP0jT6x3JxHdV5yvU9p56UMOrAvW',
+  //   typePage: 'Personal',
+  // })
+  // .fetch();
+  
+  // var client = await ClientsData.create({
+  //   idfbs: '1703497029718211',
+  //   first_name: 'Saúl',
+  //   last_name: 'Navarrov',
+  //   profile_pic: 'https://platform-lookaside.fbsbx.com/platform/profilepic/?psid=1703497029718211&width=1024&ext=1532452683&hash=AeRbgHS_iJ17V-Vu',
+  //   locale: 'es_LA',
+  //   timezone: -5,
+  //   gender: 'male',
+  // })
+  // .fetch();
+  
+  // var clientid = await ClientsId.create({
+  //     datasclients: client.id,
+  //     dataspages: page.id
+  //   })
+  //   .fetch();
   // Don't forget to trigger `done()` when this bootstrap function's logic is finished.
   // (otherwise your server will never lift, since it's waiting on the bootstrap)
   return done();
