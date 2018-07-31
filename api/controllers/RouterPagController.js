01b77413313c8a6b11e1025d6f7318ccb76e2c74
saulnavarrov <sinavarrov@gmail.com>
2018-07-31 17:03:00 -0500

    controller WebHooksController
    ORGANIZANDO LOS MENSAJES

---
+++ api/controllers/RouterPagController.js
@@ -0,0 +1,13 @@
+/**
+ * RouterPagController
+ *
+ * @description :: Controlador de todas las paginas que contenga la aplicacion
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+
+};
+
